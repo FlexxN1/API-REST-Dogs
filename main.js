5c3d9f7238d77978678dde17b1d22e89eb8f11fd
Flexx <juandavidx40@gmail.com>
2022-10-21 12:50:26 -0500

    Update

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const URL = 'https://api.thedogapi.com/v1/images/search';
 async function myCat () {
     const res = await fetch(URL);
     const data = await res.json();
-    const img = document.querySelector('img');
-    img.src = data[0].url;
+    const img = document.querySelector('#img');
+    img.src = data[0].url
 }
 
 const myButton = document.querySelector("button");
